be5eb14a3863c2a4f0e0e865ccd2fb220b6a27d2
Miguel Guerrero <miguel_alfonso_d_guerrero@dlsu.edu.ph>
2025-05-24 09:45:55 +0800

    a

--- week2_renzel/test.cpp
+++ week2_renzel/test.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
+#include <sstream>
 #include <stdlib.h>
 #include <windows.h>
+#include <ctime>
 using namespace std;
 
+// class that includes screen details
+class Screen{
+    public:
+        string screenName;
+        int curInstruction;
+        int totalInstruction;
+        string timeCreated;
+};
+
+std::vector<Screen> screenList; //global vector for list of screens (c++ can't handle dynamic sized arrays)
+
+//changes the font color of the text in the terminal
 void setColor(int color){
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+//opens and reads a file containing ASCII art for the main menu title
 void printASCII(string fileName){
-    string line = "";
-    ifstream inFile;
-    inFile.open(fileName);
+    string line = ""; //line being read
+    ifstream inFile; //input file stream
+    inFile.open(fileName); //opens the file
     if(inFile.is_open()){
         while(getline(inFile, line))
         {
-            cout << line << endl;
+            cout << line << endl; //prints file content
         }
     }
     else{
@@ -29,8 +45,51 @@ void initialize(){
     cout << "Initialize command recognized. Doing something.\n\n";
 }
 
-void screen(){
-    cout << "Screen-command recognized. Doing something.\n\n";
+void createScreen (string &screenName){
+    time_t timestamp; //make a time_t variable
+    time(&timestamp); //gets the current time
+
+    Screen newScreen; //initialize a new screen
+    newScreen.screenName = screenName;
+    newScreen.totalInstruction = rand()%50+1; //placeholder total instructions
+    newScreen.curInstruction = newScreen.totalInstruction-1; //placeholder currnet instruction, guaranteed to be less than total instruction
+    
+    char buffer[30]; //array to store new time format
+    tm* timeinfo = localtime(&timestamp); 
+    strftime(buffer, sizeof(buffer), "%m/%d/%Y, %I:%M:%S %p", timeinfo); //changes time format
+    newScreen.timeCreated = buffer;
+
+    screenList.emplace_back(newScreen); //adds screen to vector for storage
+    cout << "New screen named \"" << screenName << "\" has been created." << endl << endl;
+}
+
+void screen(string &screenCommand){
+    istringstream iss(screenCommand); //splits input separated by whitespaces
+    string screen, option, screenName;
+    iss >> screen >> option >> screenName; //stores each part of the input in its own variable
+
+    if(option == "-s" && !screenName.empty()){
+        createScreen(screenName);
+        
+        Screen& createdScreen = screenList.back(); //gets the most recently added screen for display
+        cout << "Current screen: " << createdScreen.screenName << endl;
+        cout << "Running instruction: " << createdScreen.curInstruction << "/" << createdScreen.totalInstruction << endl;
+        cout << "Time Created: " << createdScreen.timeCreated << endl << endl;
+    }
+
+    else if(option == "-r" && !screenName.empty()){
+        //TODO screen -r
+
+    }
+
+    string screenInput;
+    while(screenInput != "exit"){
+        cout << "Enter a command, or type exit to return to the main menu: ";
+        getline(cin, screenInput);
+        if(screenInput != "exit"){
+            cout << "Sorry, that command does not work right now. Only 'exit' works at the moment." << endl << endl;
+        }
+    }
 }
 
 void schedulertest(){
@@ -56,27 +115,28 @@ void intro(){
 
 void menu(){
     string fileName = "ascii.txt";
-    string command = "";
-    
-    intro();
+    string input;
 
-    while (command != "exit"){
+    while (true){
+        intro();
         setColor(7);
         cout << "Enter a command: ";
-        cin >> command;
+        string command;
+        getline(cin >> ws, command); //changed to getline to read input separated by whitespace
+
         if(command == "exit"){
             cout << "Thank you for using the program";
             exit(0);
         }
         else if (command == "clear"){
-            system("CLS");
-            intro();
+            system("CLS"); //clears the system
+            intro(); //reprints the main menu
         }
         else if(command == "initialize"){
             initialize();
         }
-        else if(command == "screen"){
-            screen();
+        else if(command.rfind("screen",0) == 0){
+            screen(command);
         }
         else if(command == "scheduler-test"){
             schedulertest();
